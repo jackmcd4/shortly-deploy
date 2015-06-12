b5e7952710a82c4076ba29b4a74a321875b8c7ba
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 20:30:48 -0700

    Final Commit, Deployed to Azure!

--- app/config.js
+++ app/config.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
-
-mongoURI = 'mongodb://MongoLab-b:ZHQTYxdwt98rvAqp3p_f4DKBt8kuySVeMAWd4Q5ce.g-@ds036648.mongolab.com:36648/MongoLab-b' || 'mongodb://localhost/shortlydb';
+mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/shortlydb';
 mongoose.connect('mongodb://localhost/shortlydb');
 
 var db = mongoose.connection;
